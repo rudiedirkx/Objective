48874afeade35f28c320a584178ac78ad3d15396
Rudie Dirkx <github@hotblocks.nl>
2013-12-25 14:18:43 +0100

    Initial (live++)

---
+++ ObjectStore.php
@@ -0,0 +1,76 @@
+<?php
+
+class ObjectStore {
+	public $file;
+	function __construct( $file = null, $load = true ) {
+		$file or defined('OBJECT_STORE_FILE') and $file = OBJECT_STORE_FILE;
+		$this->file = $file;
+
+		$load and $this->load();
+	}
+
+	function decode( $data ) {
+		$value = @json_decode($data, true);
+		if (json_last_error()) {
+			$value = array();
+		}
+		return $value;
+	}
+
+	function encode( $data ) {
+		return @json_encode($data);
+	}
+
+	function load() {
+		return $this->store = $this->decode(@file_get_contents($this->file));
+	}
+
+	function save() {
+		return file_put_contents($this->file, $this->encode($this->store));
+	}
+
+	function put( $name, $value ) {
+		$parents = explode('.', $name);
+		$child = array_pop($parents);
+
+		$container = &$this->store;
+		foreach ( $parents as $parent ) {
+			if ( !isset($container[$parent]) || !is_array($container[$parent]) ) {
+				$container[$parent] = array();
+			}
+
+			$container = &$container[$parent];
+		}
+
+		$container[$child] = $value;
+	}
+
+	function get( $name, &$found = null ) {
+		$parents = explode('.', $name);
+		$child = array_pop($parents);
+
+		$container = $this->store;
+		foreach ( $parents as $parent ) {
+			if ( !isset($container[$parent]) ) {
+				$found = false;
+				return;
+			}
+
+			$container = $container[$parent];
+		}
+
+		if ( $found = array_key_exists($child, $container) ) {
+			return $container[$child];
+		}
+	}
+
+	function output( $data, $cors = true ) {
+		$cors && header('Access-Control-Allow-Origin: *');
+		$antiHijack = 'while(1);';
+		header('Content-type: text/json');
+		header('X-script-time: ' . ((microtime(1) - _START)*1000) . ' ms');
+		header('X-anti-hijack: ' . strlen($antiHijack));
+		echo $antiHijack . json_encode($data);
+		exit;
+	}
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+define('_START', microtime(1));
+
+require __DIR__ . '/ObjectStore.php';
+
+$store = isset($_REQUEST['store']) ? preg_replace('#[^\w@\.\-]#i', '', basename($_REQUEST['store'])) : '';
+define('OBJECT_STORE_FILE', __DIR__ . '/stores/' . ( $store ?: 'default$' ) . '.json');
+// exit(OBJECT_STORE_FILE);
+
+if ( isset($_GET['debug']) ) {
+	header('Content-type: text/plain');
+	$store = new ObjectStore;
+	// Not CORS, not JSON
+	print_r($store->store);
+	exit;
+}
+
+$get = @$_REQUEST['get'];
+$put = @$_REQUEST['put'];
+$value = @$_REQUEST['value'];
+
+if ( $get ) {
+	$store = new ObjectStore;
+	$value = $store->get($get, $exists);
+
+	return $store->output(array('error' => 0, 'exists' => $exists, 'value' => $value));
+}
+
+else if ( $put && $value !== null && $value !== '' ) {
+	$store = new ObjectStore;
+
+	$value = $store->decode($value);
+	$store->put($put, $value);
+	$store->save();
+
+	return $store->output(array('error' => 0, 'value' => $value));
+}
