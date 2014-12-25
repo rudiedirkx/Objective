2ec6109a2c071f31d7f295b09833c8d55f64e47f
Rudie Dirkx <github@hotblocks.nl>
2014-12-25 17:21:54 +0100

    #4 +delete method, decoding+, store file+

--- ObjectStore.php
+++ ObjectStore.php
@@ -1,20 +1,19 @@
 <?php
 
 class ObjectStore {
-	public $file;
-	public $decodeFail = array();
 
-	function __construct( $file = null, $load = true ) {
-		$file or defined('OBJECT_STORE_FILE') and $file = OBJECT_STORE_FILE;
+	public $file = '';
+	public $store = null;
+
+	function __construct( $file, $load = true ) {
 		$this->file = $file;
-
 		$load and $this->load();
 	}
 
 	function decode( $data ) {
 		$value = @json_decode($data, true);
-		if (json_last_error()) {
-			$value = $this->decodeFail;
+		if ($error = json_last_error()) {
+			throw new Exception($error);
 		}
 		return $value;
 	}
@@ -24,11 +23,43 @@ class ObjectStore {
 	}
 
 	function load() {
-		return $this->store = $this->decode(@file_get_contents($this->file));
+		$encoded = @file_get_contents($this->file);
+		return $this->store = $encoded ? $this->decode($encoded) : array();
 	}
 
 	function save() {
 		return file_put_contents($this->file, $this->encode($this->store));
+	}
+
+	function delete( $name, $clean = false ) {
+		$parents = explode('.', $name);
+		$child = array_pop($parents);
+
+		$container = &$this->store;
+		foreach ( $parents as $parent ) {
+			// Next child doesn't exist
+			if ( !isset($container[$parent]) || !is_array($container[$parent]) ) {
+				return false;
+			}
+
+			$container = &$container[$parent];
+		}
+
+		// Last child doesn't exist
+		if ( !array_key_exists($child, $container) ) {
+			return false;
+		}
+
+		// Remove last child
+		unset($container[$child]);
+
+		// Clean up empty container (expensive)
+		if ( $clean && $container === array() && $parents ) {
+			$name = implode('.', $parents);
+			return $this->delete($name, $clean);
+		}
+
+		return true;
 	}
 
 	function put( $name, $value ) {
@@ -69,10 +100,14 @@ class ObjectStore {
 	function output( $data, $cors = true ) {
 		$cors && header('Access-Control-Allow-Origin: *');
 		$antiHijack = 'while(1);';
+
 		header('Content-type: text/json');
-		header('X-script-time: ' . ((microtime(1) - _START)*1000) . ' ms');
 		header('X-anti-hijack: ' . strlen($antiHijack));
-		echo $antiHijack . json_encode($data);
+
+		$output = $antiHijack . $this->encode($data);
+
+		header('X-script-time: ' . ((microtime(1) - _START)*1000) . ' ms');
+		echo $output;
 		exit;
 	}
 }
--- index.php
+++ index.php
@@ -7,31 +7,51 @@ require __DIR__ . '/ObjectStore.php';
 
 $store = isset($_REQUEST['store']) ? preg_replace('#[^\w@\.\-]#i', '', basename($_REQUEST['store'])) : '';
 define('OBJECT_STORE_FILE', WHERE_STORES_AT . '/' . ( $store ?: 'default$' ) . '.json');
-// exit(OBJECT_STORE_FILE);
 
 if ( isset($_GET['debug']) ) {
-	header('Content-type: text/plain');
-	$store = new ObjectStore;
+	$store = new ObjectStore(OBJECT_STORE_FILE);
+
 	// Not CORS, not JSON
+	header('Content-type: text/plain');
 	print_r($store->store);
 	exit;
 }
 
 $get = @$_REQUEST['get'];
 $put = @$_REQUEST['put'];
+$delete = @$_REQUEST['delete'];
 $value = @$_REQUEST['value'];
 
+// GET
 if ( $get ) {
-	$store = new ObjectStore;
+	$store = new ObjectStore(OBJECT_STORE_FILE);
 	$value = $store->get($get, $exists);
 
 	return $store->output(array('error' => 0, 'exists' => $exists, 'value' => $value));
 }
 
-else if ( $put && $value !== null && $value !== '' ) {
-	$store = new ObjectStore;
+// DELETE
+else if ( $delete ) {
+	$store = new ObjectStore(OBJECT_STORE_FILE);
+	$existed = $store->delete($delete, !empty($_GET['clean']));
+	if ( $existed ) {
+		$store->save();
+	}
 
-	$value = $store->decode($value);
+	return $store->output(array('error' => 0, 'existed' => $existed));
+}
+
+// PUT
+else if ( $put && $value !== null && $value !== '' ) {
+	$store = new ObjectStore(OBJECT_STORE_FILE);
+
+	try {
+		$value = $store->decode($value);
+	}
+	catch ( Exception $ex ) {
+		return $store->output(array('error' => 'decode: ' . $ex->getMessage()));
+	}
+
 	$store->put($put, $value);
 	$store->save();
 
